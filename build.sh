e6fd4ee285bc1d7cdca3ea0609ff6ddfb004e7e9
Igor Fedorenko <igor@ifedorenko.com>
2011-12-21 22:21:18 -0500

    fixed build failures
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export MAVEN_OPTS="-Xmx512m"
+
 mvn -fn clean install -f onboarding-models/pom.xml
 mvn -fn clean install -f onboarding-nexus-p2-runtime/nexus-p2-facade/pom.xml
 mvn -fn clean install -f onboarding-nexus-p2-runtime/pom.xml
